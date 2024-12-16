c6586b70a2fb7dcf8e2dd6bc643ad65f1a9ee8dc
LE PHU QUY <155272789+MooreRaby@users.noreply.github.com>
2024-12-17 03:32:39 +0700

    first commit

---
+++ internal/initialize/router.go
@@ -0,0 +1,40 @@
+package initialize
+
+import (
+	"github.com/anonystick/go-ecommerce-backend-api/global"
+	"github.com/anonystick/go-ecommerce-backend-api/internal/routers"
+	"github.com/gin-gonic/gin"
+)
+
+func InitRouter() *gin.Engine {
+	var r *gin.Engine
+	if global.Config.Server.Mode == "dev" {
+		gin.SetMode(gin.DebugMode)
+		gin.ForceConsoleColor()
+		r = gin.Default()
+	} else {
+		gin.SetMode(gin.ReleaseMode)
+		r = gin.New()
+	}
+	// middlewares
+	// r.Use() // logging
+	// r.Use() // cross
+	// r.Use() // limiter global
+	manageRouter := routers.RouterGroupApp.Manage
+	userRouter := routers.RouterGroupApp.User
+
+	MainGroup := r.Group("/v1/2024")
+	{
+		MainGroup.GET("/checkStatus") // tracking monitor
+	}
+	{
+		userRouter.InitUserRouter(MainGroup)
+		userRouter.InitProductRouter(MainGroup)
+	}
+	{
+		manageRouter.InitUserRouter(MainGroup)
+		manageRouter.InitAdminRouter(MainGroup)
+	}
+
+	return r
+}
